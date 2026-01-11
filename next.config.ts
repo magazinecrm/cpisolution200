6fc31d1f323d7645a773dd98def7e2671fb37657
magazinecrm <paul@magazinecrm.com>
2026-01-11 23:31:55 +0530

    Redirect /mentalhealth to /mental-health

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/mentalhealth",
+        destination: "/mental-health",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
